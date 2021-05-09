61fe7607500213f57810d9d8c7f3c33f0980b7ec
Aten <gogo.june2005@gmail.com>
2021-05-09 21:45:11 +0530

    amogus

--- main.py
+++ main.py
@@ -16,19 +16,25 @@ import discord
 from discord.ext import commands, tasks
 from discord.utils import get
 import json
+import requests
 
 DELAY = 5
 TIMEOUT = 60
 
 chrome_options = Options()
 chrome_options.add_argument('--log-level=3')
-# chrome_options.add_argument("--headless")
-chrome_options.add_argument("--user-data-dir=D:\One for All\PyProjects\ScheduleGrabber\data")
+chrome_options.add_argument("--headless")
 # chrome_options.add_argument("--profile-directory=.\data\Default")
-chrome_options.add_argument('--profile-directory=Debug')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options= chrome_options)
+
+def get_link(string:str):
+    begin = string.find("https://")
+    string = string[begin:]
+    string = string.splitlines()[0]
+    return string
+
 
 def get_prefix(client, message):
     if not message.guild:
@@ -108,6 +114,93 @@ async def schedule(ctx):
     with open("users.json", "r") as f:
         users = json.load(f)
 
+    if str(ctx.author.id) in users:
+        USER_ID = users[str(ctx.author.id)]["ID"]
+        USER_PASS = users[str(ctx.author.id)]["PASS"]
+    else:
+        await ctx.send(f"No profile found for user {ctx.author.name}. Try using .register")
+        return
+
+    links = []
+
+    driver.get("http://dpskolkata.net/")
+
+    #login code
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_txtusername")))
+    username = driver.find_element_by_id("ContentPlaceHolder1_txtusername")
+    username.send_keys(Keys.CONTROL + "A", Keys.BACKSPACE)
+    username.send_keys(USER_ID)
+
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_txtpwd")))
+    password = driver.find_element_by_id("ContentPlaceHolder1_txtpwd")
+    password.send_keys(Keys.CONTROL + "A", Keys.BACKSPACE)
+    password.send_keys(USER_PASS)
+
+
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_imgCaptcha")))
+    captcha = driver.find_element_by_id("ContentPlaceHolder1_imgCaptcha")
+    src = captcha.get_attribute("src")        
+
+    img_captcha_base64 = driver.execute_async_script("""
+    var ele = arguments[0], callback = arguments[1];
+    ele.addEventListener('load', function fn(){
+    ele.removeEventListener('load', fn, false);
+    var cnv = document.createElement('canvas');
+    cnv.width = this.width; cnv.height = this.height;
+    cnv.getContext('2d').drawImage(this, 0, 0);
+    callback(cnv.toDataURL('image/jpeg').substring(22));
+    }, false);
+    ele.dispatchEvent(new Event('load'));
+    """, captcha)
+
+    with open(r"captcha.jpg", 'wb') as f:
+        f.write(base64.b64decode(img_captcha_base64))
     
+
+    await ctx.send("Type out the following captcha: ", file=discord.File("./captcha.jpg"))
+    os.remove("./captcha.jpg")
+
+    def check(message):
+        return ctx.author.id == message.author.id
+
+    try:
+        message = await client.wait_for("message", check=check, timeout=30)
+        captcha_text = message.content
+
+    except TimeoutError:    
+        await ctx.send("Request timed out") 
+        return
+        
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_txtCaptcha")))
+        captcha_field = driver.find_element_by_id("ContentPlaceHolder1_txtCaptcha")
+        captcha_field.send_keys(captcha_text)
+
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_btnLogin")))
+        login = driver.find_element_by_id("ContentPlaceHolder1_btnLogin")
+        login.click()
+
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "ContentPlaceHolder1_ContentPlaceHolder2_btnschedule")))
+        schedule = driver.find_element_by_id("ContentPlaceHolder1_ContentPlaceHolder2_btnschedule")
+        schedule.click()
+
+        rows = driver.find_elements_by_xpath("//tbody/tr")
+        for row in rows:
+            segments = row.find_elements_by_xpath(".//td")
+            if len(segments) != 0:
+                link = f"[{segments[-6].text}][{segments[-5].text}][{segments[-4].text}] Link: {get_link(segments[-3].text)}\n"
+                links.append(link)
+
+        message = ""
+        for link in links:
+            message += link
+        time.sleep(1)
+    
+        if message == "":
+            await ctx.send(f"No schedule found for user {ctx.author.name}")
+            return
+
+        await ctx.send(message)
+
+
 
 client.run("ODQwNTQ1MjY1ODg2NjkxMzMw.YJZwxw.OdIeTWco4zMW1Tqs1rlADwljYFE")
